ad10eba5f62abfbd5898198361f67e14e56ed1ed
Guido Teunissen <iguido@me.com>
2015-10-29 12:40:21 +0100

    Edited endpoint for AWS

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,7 +4,6 @@ if Rails.env.production?
 			:provider => 'AWS',
 			:aws_access_key_id => ENV['S3_ACCESS_KEY'],
 			:aws_secret_access_key => ENV['S3_SECRET_KEY'],      
-			:endpoint               => "https://railsrecipesapp.s3.amazonaws.com",
       :region                 => ENV['S3_REGION'] 
 		}
 
